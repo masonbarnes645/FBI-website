3ab89f119e1c31da3a4d185892c95e868d8cbd09
Mason Barnes <barnesmason980@gmail.com>
2024-06-13 11:14:24 -0700

    add catch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 function getData(url) {
     fetch(url)
     .then(res => res.json())
-    .then(console.log)}
+    .then(people => people.forEach(extractInfo))
+    .catch(err => console.log(err))
+}
 getData('https://api.fbi.gov/wanted/v1/list')
